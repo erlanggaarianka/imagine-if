8f249425d94e17fcd07f11bc218168a057e1227c
aryankawibowo <aryankawibowo@gmail.com>
2024-06-08 23:58:34 +0700

    Middleware tweak"

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,10 @@
-import { clerkMiddleware } from '@clerk/nextjs/server';
+import { authMiddleware } from "@clerk/nextjs/server";
 
-export default clerkMiddleware()
+
+export default authMiddleware({
+  ignoredRoutes: ["/api/webhooks(.*)"],
+});
 
 export const config = {
-  // The following matcher runs middleware on all routes
-  // except static assets.
-  matcher: [ '/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
 };
